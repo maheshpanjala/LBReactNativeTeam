d1d627377206d9a43705cd3ae5af9ed09abfd39f
Krishnakumar <ponna.doctutorials@outlook.com>
2025-05-26 22:20:41 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import HomeScreen from './src/Screens/homeScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
