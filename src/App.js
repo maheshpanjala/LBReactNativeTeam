0254a37c31602ba7b683a3631470dd3d1de5e94b
ADDAGALLANARESH <elonnaresh@gmail.com>
2025-05-28 17:53:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Alert} from 'react-native';
-import ReusableForm from './Components/LBButton';
+import ReusableForm from './components/ReusableForm';
 
 const App = () => {
   const handleFormSubmit = ({text, checked}) => {
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#fff',
     borderRadius: 10,
-    elevation: 2,
+    elevation:  2,
   },
 });
  
