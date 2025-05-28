5e896ae81c8472901e7125307f602311dabb4ecb
Srikanth-ai625 <srikanth.k@longbowtechnologies.com>
2025-05-28 12:04:21 +0530

    Components added and updated

--- src/styles/style.js
+++ src/styles/style.js
@@ -1,9 +1,43 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-const style = StyleSheet.create({
-    text:{
-        color:'orange',
-    }
-})
+const commonStyles = StyleSheet.create({
 
-export default style;
+  inputContainer: {
+    flex: 0,
+    padding: 0,
+    backgroundColor: '#fff',
+    marginTop:30,
+  },
+  inputLabel: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: '#333',
+  },
+  inputField: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  checkboxContainer: {
+    marginVertical: 10,
+  },
+  checkboxLabel: {
+    fontSize: 16,
+    color: '#333',
+  },
+});
+
+export default commonStyles;
