f8cc330cd041987a76e0f8fd6d6e755801bbc1b9
ADDAGALLANARESH <elonnaresh@gmail.com>
2025-06-04 15:13:31 +0530

    NAVIGATION

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-// App.js
-import * as React from 'react';
+import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './components/HomePage';
-import DetailsScreen from './components/NewPaage';
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+import HomeScreen from './Components/HomeScreen';
+import ProfileScreen from './Components/ProfileScreen';
+import UserScreen from './Components/UserScreen';
+ 
+function App() {
+  const Stack = createNativeStackNavigator();
+ 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+     <Stack.Navigator
+      initialRouteName="LongBow Tech"
+      screenOptions={{
+          statusBarColor: 'orange',
+          headerStyle: {backgroundColor: 'orange'},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+        }}
+      >
+  <Stack.Screen name="LongBow Tech" component={HomeScreen} options={{ }} />
+  <Stack.Screen name="Profiles" component={ProfileScreen} />
+  <Stack.Screen name="Personal Info" component={UserScreen} />
+</Stack.Navigator>
+ 
     </NavigationContainer>
   );
 }
+
+export default App;
