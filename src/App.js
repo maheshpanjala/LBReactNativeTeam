4b1b543ac0a14ae014e08df6b431506b13ec40c6
Krishnakumar <ponna.doctutorials@outlook.com>
2025-06-04 16:12:13 +0530

    Login,home,register & logout screens added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import {SafeAreaView, Alert} from 'react-native';
-import ReusableForm from './Components/LBButton';
+// import React from 'react';
+// import {SafeAreaView} from 'react-native';
+// import RegistrationForm from './Screens/Registration';
+
+// const App = () => {
+//   return (
+//     <SafeAreaView style={{flex: 1}}>
+//       <RegistrationForm />
+//     </SafeAreaView>
+//   );
+// };
+
+// export default App;
+
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import LoginScreen from './Screens/LoginScreen';
+import BottomTabs from './Components/BottomTabs';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const handleFormSubmit = ({text, checked}) => {
-    Alert.alert('Form Submitted', `Text: ${text}, Checked: ${checked}`);
-  };
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', padding: 20}}>
-      <ReusableForm onSubmit={handleFormSubmit} />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          {!isLoggedIn ? (
+            <Stack.Screen name="Login">
+              {props => (
+                <LoginScreen {...props} setIsLoggedIn={setIsLoggedIn} />
+              )}
+            </Stack.Screen>
+          ) : (
+            <Stack.Screen name="Main">
+              {props => <BottomTabs {...props} setIsLoggedIn={setIsLoggedIn} />}
+            </Stack.Screen>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
